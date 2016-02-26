d3821c6025c54a9e614d4c58e6bd3c8fd1e4880e
stevenjc <steven.j.colon@gmail.com>
2016-02-25 23:12:04 -0500

    Got basic signin/signup functionality working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
-class UsersController < ApplicationController
+class UsersController < Clearance::UsersController
+include Clearance::Controller
+# Prevent CSRF attacks by raising an exception.
+# For APIs, you may want to use :null_session instead.
+protect_from_forgery with: :exception
 end

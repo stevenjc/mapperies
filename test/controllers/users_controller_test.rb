da143636ebe884be9066b34084dc2e55f1c612be
Tifara Ramelson <tifararamelson@gmail.com>
2016-03-02 12:59:17 -0500

    other changes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,53 +5,22 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
+  
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: { name: 'sam', password: 'secret', password_confirmation: 'secret' }
-    end
-
-    assert_redirected_to users_path
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch :update, id: @user, user: { name: @user.name, password: 'secret', password_confirmation: 'secret' }
-    assert_redirected_to users_path
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
-
-    assert_redirected_to users_path
-  end
-
-  #added tests
-  test "should create user" do
+  #18.2-Testability: The three added tests
+  test "should create user with password" do
     assert_difference('User.count') do
       post :create, user: { name: 'Bob', password: 'Joe123', password_confirmation: 'Joe123' }
     end
   end
-  
+
+  test "after updating user, changes should be made" do
+    post :update, id: @user, user: { name: @user.name, password: 'joeshmoe', password_confirmation: 'joeshmoe' }
+    assert_redirected_to users_path
+  end
+
+  test "should show the user" do
+      get :show, id: @user
+      assert_response :success
+  end
 end

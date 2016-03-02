3c84d4864ecc061c74519543bae05d1967542f48
Jiaqi Li <jiaqli4@brandeis.edu>
2016-03-02 13:47:20 -0500

    adding back seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+Photo.delete_all
+Album.delete_all
+
+20.times do
+  User.create(name: Faker::Name.name, password_digest: Faker::Number.number(6))
+  Friend.create(user_id: User.all.sample)
+end
+
+
+100.times do
+  Photo.create(xcoord: Faker::Address.latitude, ycoord: Faker::Address.longitude,
+  album_id: Album.all.sample)
+end
+
+20.times do
+  Album.create(album_name: Faker::Address.country,
+  user_id: User.all.sample)
+end
+
+3.times do
+  Album_view.create(album_id: Album.all.sample, user_id: User.all.sample)
+end

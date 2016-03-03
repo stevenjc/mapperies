160cfee7ba1098dde979131c6a6288a090980654
stevenjc <steven.j.colon@gmail.com>
2016-03-03 15:01:03 -0500

    Made email unique, Started writing tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "should save" do
+    user = users(:one)
+    assert user.save
+
+  end
+
+  test "shouldn't save" do
+    user = users(:one)
+    user.save
+    assert_not user.save
+  end
 end

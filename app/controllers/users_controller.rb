88b747b802a84030e78963336c5575e42c695841
Jiaqi Li <jiaqli4@brandeis.edu>
2016-03-03 16:05:21 -0500

    finish restructuring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < Clearance::UsersController
-include Clearance::Controller
+#include Clearance::Controller
 # Prevent CSRF attacks by raising an exception.
 # For APIs, you may want to use :null_session instead.
 protect_from_forgery with: :exception

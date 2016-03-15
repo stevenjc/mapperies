33f44154973d640f74b3b7a2a9f0bdb1f7340966
Jiaqi Li <jiaqli4@brandeis.edu>
2016-03-15 16:47:20 -0400

    adding user_name

--- db/migrate/20160315201510_combine_user_names.rb
+++ db/migrate/20160315201510_combine_user_names.rb
@@ -1,8 +1,6 @@
 class CombineUserNames < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
-      t.remove :f_name
-      t.remove :l_name
       t.string :user_name
     end
   end

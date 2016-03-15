a89d30ed25e48d8835666ae17c940bf7177798e0
stevenjc <steven.j.colon@gmail.com>
2016-03-15 11:12:35 -0400

    Created better looking error messages during registration

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
   def error_list(attribute)
       if @user.errors.include?(attribute)
-          @user.errors.get(attribute)
+          @user.errors[attribute].to_a
       else
           []
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ActiveRecord::Base
   has_many :friendee_friends, foreign_key: :friender_id, class_name: "Friend"
   has_many :friendees, through: :friendee_friends, source: :friendee
 
-  validates :password, length: {minimum: 6, too_short: "Password must be at least six characters long"}
-  validates :email, uniqueness: true
+  validates :password, length: {minimum: 6, too_short: "must be at least six characters long"}
 end

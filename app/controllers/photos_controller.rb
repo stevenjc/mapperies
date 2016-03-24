006f623fb9b4d964bfd4912c0ba4b1a044e3f38a
Ted McNulty <this@brandeis.edu>
2016-03-24 16:54:51 -0400

    Adding paperclip, still need to play with routes and views in /views/photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,44 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all(:limit =>15)
+  end
+
+  def show
+    @photos = Photo.find(params[:id])
+  end
+
+  def new
+    @photos = Photo.new
+  end
+
+  def create
+    @photos = Photo.new(params[:photo])
+    if @photos.save
+      flash[:notice] = "Successfully uploaded photo"
+      redirect_to @photos
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+    @photos = Photo.find(params[:id])
+  end
+
+  def update
+    @photos = Photo.find(params[:id])
+    if @photos.update_attributes(params[:photo])
+      flash[:notice] = "Successfully updated photo"
+      redirect_to @photos
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @photos = Photo.find(params[:id])
+    @photos.destroy
+    flash[:notice] = "Successfully deleted photo"
+    redirect_to photos_url
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,10 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
+
+  has_attached_file :image, :styles => {:small => "150x150>"},
+
+  validates_attachment_presence :image
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+
 end

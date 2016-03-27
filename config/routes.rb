eff7cc84457744d383206285b75c7575f2d1c6ca
Tifara Ramelson <tifararamelson@gmail.com>
2016-03-27 12:32:46 -0400

    user can add friend (still need acceptance, avoid adding self, display on friends page properly)

--- config/routes.rb
+++ config/routes.rb
@@ -41,9 +41,13 @@ Rails.application.routes.draw do
 
   get '/albums', to:"albums#index"
 
+  get 'friends/index'
   get '/friends', to:"friends#index"
-  post '/friends/results', to:"friends#results"
+  post '/friends/index'
+  post '/friends', to:"friends#index"
   get '/friends/results', to:"friends#results"
+  post '/friends/results', to:"friends#results"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

6128a4c95cf0b2deb830284d8d9bad2757ce25ac
stevenjc <steven.j.colon@gmail.com>
2016-03-31 16:38:06 -0400

    Changed main page UI | restructured some files

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,10 @@
 module MainHelper
+
+    def user_identifier
+        if current_user.user_name
+            return current_user.user_name
+        else
+            return current_user.email
+        end
+    end
 end

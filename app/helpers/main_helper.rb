753f661fc8f4307039a7f87ba5e209668d0eecc6
stevenjc <steven.j.colon@gmail.com>
2016-04-01 12:48:21 -0400

    Edit account page UI changed

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,8 +3,10 @@ module MainHelper
     def user_identifier
         if current_user.user_name
             return current_user.user_name
+        elsif current_user.first_name
+            return current_user.first_name
         else
-            return current_user.email
+            current_user.first_name
         end
     end
 end

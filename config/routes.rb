84e6ece1297d8fec5cf17638d602230d4d361e46
Steven <stevenjc@brandeis.edu>
2016-04-09 17:30:57 -0400

    Implemented slick carosel for Friends list album view | Doesn't Use actual pictures yet

--- config/routes.rb
+++ config/routes.rb
@@ -66,6 +66,7 @@ Rails.application.routes.draw do
   post '/friends/results', to:"friends#results"
 
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

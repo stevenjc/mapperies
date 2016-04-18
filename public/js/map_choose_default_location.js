a2f2f856bb33686e2df5e4ae2616ac874cad5ff3
Ted McNulty <this@brandeis.edu>
2016-04-18 18:12:16 -0400

    Show map in albums/show.index to set default location

--- public/js/map_choose_default_location.js
+++ public/js/map_choose_default_location.js
@@ -1,31 +1,36 @@
 
 var map;
 var brandeisCoords = {lat:42.3657, lng:-71.2597};
-var marker;
+var default_marker = "../img/marker_blue.png";
+var new_marker = "../img/marker_green.png";
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:42.3657, lng:-71.2597},
-    zoom: 16
+    zoom: 4
   });
 
   var default_loc =  new google.maps.Marker({
     position: brandeisCoords,
-    map:map
+    map:map,
+    icon:default_marker
+  })
+  var new_loc = new google.maps.Marker({
+    position:brandeisCoords,
+    map:map,
+    icon:new_marker
   })
 
-  map.addListener('click', function(){
-    marker = new google.maps.Marker({
-      position: mouse.getPosition(),
-      map:map
-    })
+  map.addListener('click', function(e){
+  //  alert(e.latLng);
+    new_loc.setPosition(e.latLng);
   })
 
 
 }
 
 function placeMarker(position, map){
-  var marker = google.maps.Marker({
+  var marker = new google.maps.Marker({
     position: position,
     map:map
   });

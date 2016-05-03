5c4f76743e19eb106fe84c53ee0df7889b5c098e
Steven <stevenjc@brandeis.edu>
2016-05-03 17:46:16 -0400

    Fixed Sample Picture Error

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,6 +1,6 @@
 module MainHelper
-
     def user_identifier
+      if current_user
         if current_user.user_name
             return current_user.user_name
         elsif current_user.first_name
@@ -8,5 +8,6 @@ module MainHelper
         else
             return current_user.email
         end
+      end
     end
 end

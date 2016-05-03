dbb34bd5b650da39cd2761ddaed23188d1a84852
Tifara Ramelson <tifararamelson@gmail.com>
2016-05-03 09:56:31 -0400

    deletes test

--- test/controllers/friends_controller_test.rb
+++ test/controllers/friends_controller_test.rb
@@ -2,8 +2,4 @@ require 'test_helper'
 
 class FriendsControllerTest < ActionController::TestCase
 
-   test "should require login" do
-    get :index
-    response.should redirect_to sign_in_path
-   end
 end

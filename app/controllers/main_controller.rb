65a9d0a6cae15a273ddc938bdb15907c46f7ec65
Steven <stevenjc@brandeis.edu>
2016-05-03 02:27:19 -0400

    Fixed error| missing end

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -32,45 +32,45 @@ class MainController < ApplicationController
     end
 
 
-  # For each album, push all the photos and links into the arrays
-  album_ids.each do |i|
-    photo_ids = Photo.where(:album_id => Integer(i))
-    photo_ids.each do |t|
-      tempalbum = Album.find(Integer(i))
-      #only show unmapped photos that belong to the user
-      if (t.x_coord == nil) && (tempalbum.user_id==current_user.id)
-        @unmapped.push(t)
-        @unmapped_album.push(i);
-      #ignore unmapped photos of the other users
-      elsif (t.x_coord ==nil)
-      #For everything else, save the url, coordinates, and which album its from
-      else
-        links.push(t.image.url)
-        x_coord.push(t.x_coord)
-        y_coord.push(t.y_coord)
-        albums.push(Integer(i))
+    # For each album, push all the photos and links into the arrays
+    album_ids.each do |i|
+      photo_ids = Photo.where(:album_id => Integer(i))
+      photo_ids.each do |t|
+        tempalbum = Album.find(Integer(i))
+        #only show unmapped photos that belong to the user
+        if (t.x_coord == nil) && (tempalbum.user_id==current_user.id)
+          @unmapped.push(t)
+          @unmapped_album.push(i);
+        #ignore unmapped photos of the other users
+        elsif (t.x_coord ==nil)
+        #For everything else, save the url, coordinates, and which album its from
+        else
+          links.push(t.image.url)
+          x_coord.push(t.x_coord)
+          y_coord.push(t.y_coord)
+          albums.push(Integer(i))
+        end
       end
-    end
-    i = 0
-    color_map = Hash.new
-    albums.each do |a|
-      if (color_map[a]==nil)
-        color_map[a]=i;
-        color.push(i);
-        i=i+1;
-      else
-        color.push(color_map[a])
+      i = 0
+      color_map = Hash.new
+      albums.each do |a|
+        if (color_map[a]==nil)
+          color_map[a]=i;
+          color.push(i);
+          i=i+1;
+        else
+          color.push(color_map[a])
+        end
       end
+
+      gon.x = x_coord
+      gon.y = y_coord
+      gon.img = links
+      gon.albums = albums
+      gon.unmapped = @unmapped
+      gon.color = color
+
     end
-
-    gon.x = x_coord
-    gon.y = y_coord
-    gon.img = links
-    gon.albums = albums
-    gon.unmapped = @unmapped
-    gon.color = color
-
-
   end
 
   def form

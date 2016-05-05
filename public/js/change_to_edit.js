e88e030e517c1785b5666149f7adb9d002668b3a
Steven <stevenjc@brandeis.edu>
2016-05-05 14:19:01 -0400

    Can change Album Name

--- public/js/change_to_edit.js
+++ public/js/change_to_edit.js
@@ -1,9 +1,11 @@
-function edit(allowed){
-  if(allowed){
+function edit(restricted){
+  if(! restricted){
     // Grab the album name field, its parent tag and create an input value
     // var parent = document.getElementById("album_name_parent");  //div which hold the name field
     // var button = document.getElementById("edit_name");
-    document.getElementById("change_notice").innerHTML= "";
+    if (document.getElementById("change_notice")){
+      document.getElementById("change_notice").innerHTML= "";
+    }
     var field = document.getElementById("album_name");
     var name = document.getElementById("album_name").innerHTML;
     //Check that name field isn't already editing
@@ -33,7 +35,6 @@ function edit(allowed){
             name.innerHTML=input;
             n.value=input;
 
-
             submit.click();
             //NEED TO DO ACTUAL INPUT SUBMISSION
           }
@@ -42,27 +43,9 @@ function edit(allowed){
     }
   }
 }
-  //}
-  //Change the buttons back to normal and submit the form to update the
-  //name in the database
-  // else if (button.innerHTML=="Submit") {
-  //   var content = iframeRef(document.getElementById("ifrm"));
-  //
-  //   var n= content.getElementById("a_name");
-  //   var name = document.createElement("P");   //!!!!!!!!!!!!!!!!
-  //   var submit = content.getElementById("submit_album_name_edit");
-  //   name.id="album_name";
-  //   var input = document.getElementById("input_name");
-  //
-  //   name.innerHTML=input.value;
-  //   n.value=input.value;
-  //
-  //   while (parent.hasChildNodes()){
-  //     parent.removeChild(parent.firstChild);
-  //   }
-  //
-  //   parent.appendChild(name, parent.childNodes[0]);
-  //
-  //   button.innerHTML="Edit";
-  //   submit.click();
-  // }
+
+function iframeRef( frameRef ) {
+    return frameRef.contentWindow
+        ? frameRef.contentWindow.document
+        : frameRef.contentDocument
+}

54b71b2e966821407184566bde443777ff1a022a
Steven <stevenjc@brandeis.edu>
2016-05-05 02:21:57 -0400

    Working on Changing Albums Name | Have text field working but need to do submission

--- public/js/change_to_edit.js
+++ public/js/change_to_edit.js
@@ -3,50 +3,48 @@ function edit(){
   var parent = document.getElementById("album_name_parent");  //div which hold the name field
   var button = document.getElementById("edit_name");
 
-
+  var field = document.getElementById("album_name");
+  var name = document.getElementById("album_name").innerHTML;
   //Change the buttons to allow user to type where album name was
-  if(button.innerHTML=="Edit"){
-    var name = document.getElementById("album_name");
-    var input = document.createElement("input");
-
-    // Make the new input value a text input
-    input.type="text";
-    input.value = name.innerHTML;
-    input.id = "input_name";
-
-    // Print the value in the box for texting
-    // alert(name.innerHTML);
-
-    // Remove all the children in the form
-    while (parent.hasChildNodes()){
-      parent.removeChild(parent.firstChild);
-    }
-    // Add in the new text field
-    parent.appendChild(input, parent.childNodes[0]);
-    button.innerHTML="Submit";
-
-  }
-  //Change the buttons back to normal and submit the form to update the
-  //name in the database
-  else if (button.innerHTML=="Submit") {
-    var content = iframeRef(document.getElementById("ifrm"));
-
-    var n= content.getElementById("a_name");
-    var name = document.createElement("P");   //!!!!!!!!!!!!!!!!
-    var submit = content.getElementById("submit_album_name_edit");
-    name.id="album_name";
-    var input = document.getElementById("input_name");
-
-    name.innerHTML=input.value;
-    n.value=input.value;
-
-    while (parent.hasChildNodes()){
-      parent.removeChild(parent.firstChild);
-    }
-
-    parent.appendChild(name, parent.childNodes[0]);
-
-    button.innerHTML="Edit";
-    submit.click();
+  if(field.class != "editing"){
+    field.class="editing";
+    document.getElementById("album_name").innerHTML = '<input type="text" id="input_name" placeholder="'+ field.innerHTML+'">';
+    var new_name = document.getElementById("input_name");
+    $("input").focus();
+    $("input").keyup(function(e){
+      if (e.which == 13) {
+        if (new_name.value==""){
+          document.getElementById("album_name").innerHTML= name
+        }else {
+          document.getElementById("album_name").innerHTML= new_name.value
+        }
+        field.class="";
+        //NEED TO DO ACTUAL INPUT SUBMISSION
+      }
+    });
   }
 }
+  //}
+  //Change the buttons back to normal and submit the form to update the
+  //name in the database
+  // else if (button.innerHTML=="Submit") {
+  //   var content = iframeRef(document.getElementById("ifrm"));
+  //
+  //   var n= content.getElementById("a_name");
+  //   var name = document.createElement("P");   //!!!!!!!!!!!!!!!!
+  //   var submit = content.getElementById("submit_album_name_edit");
+  //   name.id="album_name";
+  //   var input = document.getElementById("input_name");
+  //
+  //   name.innerHTML=input.value;
+  //   n.value=input.value;
+  //
+  //   while (parent.hasChildNodes()){
+  //     parent.removeChild(parent.firstChild);
+  //   }
+  //
+  //   parent.appendChild(name, parent.childNodes[0]);
+  //
+  //   button.innerHTML="Edit";
+  //   submit.click();
+  // }

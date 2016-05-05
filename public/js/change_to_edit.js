7db592103d317d1e411d74aa36bc967d1f136431
Steven <stevenjc@brandeis.edu>
2016-05-05 11:26:09 -0400

    Only Users with proper permissions can change album title

--- public/js/change_to_edit.js
+++ public/js/change_to_edit.js
@@ -1,43 +1,45 @@
-function edit(){
-  // Grab the album name field, its parent tag and create an input value
-  // var parent = document.getElementById("album_name_parent");  //div which hold the name field
-  // var button = document.getElementById("edit_name");
-  document.getElementById("change_notice").innerHTML= "";
-  var field = document.getElementById("album_name");
-  var name = document.getElementById("album_name").innerHTML;
-  //Check that name field isn't already editing
-  if(field.class != "editing"){
-    field.class="editing";  //Mark field as editing
-    //Replace Name with input field
-    document.getElementById("album_name").innerHTML = '<input type="text" id="input_name" placeholder="'+ field.innerHTML+'">';
-    var new_name = document.getElementById("input_name");
-    //User cursor moved into input field and submission occurs when enter key is hit
-    $("input").focus();
-    $("input").keyup(function(e){
-      if (e.which == 13) {
-        if (new_name.value==""){
-          document.getElementById("album_name").innerHTML= name;
-          field.class="";
-        }else {
-          field.class="";
-          document.getElementById("album_name").innerHTML= new_name.value;
-          var content = iframeRef(document.getElementById("ifrm"));
+function edit(allowed){
+  if(allowed){
+    // Grab the album name field, its parent tag and create an input value
+    // var parent = document.getElementById("album_name_parent");  //div which hold the name field
+    // var button = document.getElementById("edit_name");
+    document.getElementById("change_notice").innerHTML= "";
+    var field = document.getElementById("album_name");
+    var name = document.getElementById("album_name").innerHTML;
+    //Check that name field isn't already editing
+    if(field.class != "editing"){
+      field.class="editing";  //Mark field as editing
+      //Replace Name with input field
+      document.getElementById("album_name").innerHTML = '<input type="text" id="input_name" placeholder="'+ field.innerHTML+'">';
+      var new_name = document.getElementById("input_name");
+      //User cursor moved into input field and submission occurs when enter key is hit
+      $("input").focus();
+      $("input").keyup(function(e){
+        if (e.which == 13) {
+          if (new_name.value==""){
+            document.getElementById("album_name").innerHTML= name;
+            field.class="";
+          }else {
+            field.class="";
+            document.getElementById("album_name").innerHTML= new_name.value;
+            var content = iframeRef(document.getElementById("ifrm"));
 
-          var n= content.getElementById("a_name");
-        //  var name = document.createElement("P");
-          var submit = content.getElementById("submit_album_name_edit");
-          name.id="album_name";
-          var input = new_name.value;
+            var n= content.getElementById("a_name");
+          //  var name = document.createElement("P");
+            var submit = content.getElementById("submit_album_name_edit");
+            name.id="album_name";
+            var input = new_name.value;
 
-          name.innerHTML=input;
-          n.value=input;
+            name.innerHTML=input;
+            n.value=input;
 
 
-          submit.click();
-          //NEED TO DO ACTUAL INPUT SUBMISSION
+            submit.click();
+            //NEED TO DO ACTUAL INPUT SUBMISSION
+          }
         }
-      }
-    });
+      });
+    }
   }
 }
   //}
